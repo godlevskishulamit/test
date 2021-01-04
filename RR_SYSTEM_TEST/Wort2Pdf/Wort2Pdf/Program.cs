92832b0fb7d2163918379bd0f4ad4cbd35f8aa9a
shulamit+godlevski <shligu@gmail.com>
2021-01-04 11:47:23 +0200

    test project

---
+++ RR_SYSTEM_TEST/Wort2Pdf/Wort2Pdf/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+
+namespace Wort2Pdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+        
+            
+                
+
+                Document document = new Document();
+                document.LoadFromFile(@"C:\data\past2.docx");
+
+                //Convert Word to PDF
+                document.SaveToFile("toPDF.PDF", FileFormat.PDF);
+
+                //Launch Document
+                //System.Diagnostics.Process.Start("toPDF.PDF");
+            }
+        }
+    }
+    
+
+    
+
